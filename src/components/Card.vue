ab008f4a9add6c27a23c550b0d18362d86e25a5f
vittosba <vitsbardella@gmail.com>
2021-12-02 16:02:44 +0100

    add overlay on cards

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,18 +1,18 @@
 <template>
   <div class="card">
-      <ul>
-          <li>Titolo: {{ title }}</li>
-          <li v-if="poster !== null">
-              <img :src="`https://image.tmdb.org/t/p/w500${poster}`" alt="">
-            </li>
-          <li>Titolo Originale: {{ originalTitle }}</li>
-          <li>
-              Lingua: 
-              <img v-if="isFlag" :src="require(`../assets/${language}.png`)" :alt="language">
-              <span v-else>{{ language }}</span>
-            </li>
-          <li>Voto: {{ vote }}</li>
-      </ul>
+      <img v-if="poster !== null" :src="`https://image.tmdb.org/t/p/w342${poster}`" alt="">
+      <div class="overlay">
+        <ul>
+            <li>Titolo: {{ title }}</li>
+            <li>Titolo Originale: {{ originalTitle }}</li>
+            <!-- <li>
+                Lingua: 
+                <img v-if="isFlag" :src="require(`../assets/${language}.png`)" :alt="language">
+                <span v-else>{{ language }}</span>
+                </li> -->
+            <li>Voto: <i v-for="n in stars(vote)" :key="`fullStar-${n}`" class="fas fa-star"></i><i v-for="n in 5-stars(vote)" :key="`emptyStar-${n}`" class="far fa-star"></i>  </li>
+        </ul>
+      </div>
   </div>
 </template>
 
@@ -35,14 +35,42 @@ export default {
         isFlag() {
             return this.availablesFlags.includes(this.language);
         }
+    },
+    methods: {
+        stars(number) {
+            if(Math.ceil(number)/2) {
+                return parseInt(Math.ceil(number)/2);
+            }
+            return 0;
+        }
     }
 }
+
 </script>
 
 <style scope lang="scss">
     .card {
-        img {
-            width: 30px;
+        position: relative;
+
+        .overlay {
+            position: absolute;
+            width: 100%;
+            height: 100%;
+            opacity: 0;
+            background: rgba($color: #000000, $alpha: .8);
+            transition: opacity .5s;
+            padding: 1rem;
+        }
+        ul {
+            list-style: none;
+            color: #fff;
+        }
+
+        &:hover {
+            .overlay {
+                 opacity: 1;
+
+            }
         }
     }
 </style>
--- src/components/MoviesList.vue
+++ src/components/MoviesList.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="container">
-      <div v-for="movie in movies" :key="`movie-${movie.id}`">
-          <Card 
-            :title="movie.title"
-            :poster="movie.poster_path"
-            :originalTitle="movie.title"
-            :language="movie.original_language"
-            :vote=" movie.vote_average"
-          />
-      </div>
-      <div v-for="tv in tvs" :key="`tv-${tv.id}`">
-          <Card 
-            :title="tv.name"
-            :poster="tv.poster_path"
-            :originalTitle="tv.original_name"
-            :language="tv.original_language"
-            :vote=" tv.vote_average"
-          />
+      <div class="row">
+        <div class="col-4" v-for="movie in movies" :key="`movie-${movie.id}`">
+            <Card 
+                :title="movie.title"
+                :poster="movie.poster_path"
+                :originalTitle="movie.title"
+                :language="movie.original_language"
+                :vote=" movie.vote_average"
+            />
+        </div>
+        <div class="col-4" v-for="tv in tvs" :key="`tv-${tv.id}`">
+            <Card 
+                :title="tv.name"
+                :poster="tv.poster_path"
+                :originalTitle="tv.original_name"
+                :language="tv.original_language"
+                :vote=" tv.vote_average"
+            />
+        </div>
       </div>
   </div>
 </template>
